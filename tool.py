1a7a312360aac2e2089f8da00d4f89c1a572f71b
mod3sty <marco.yang.ca@gmail.com>
2023-02-08 22:40:00 -0800

    working record form, python script lists bfs comp

--- tool.py
+++ tool.py
@@ -1,6 +1,7 @@
-#!/opt/homebrew/Caskroom/miniconda/base/bin/python
+#!/bin/python3
 
 import json
+import functools
 
 # Dictionary stores records for a player
 records = {}
@@ -22,7 +23,7 @@ def win_count(player):
     return count
 
 # Take in records from a spreadsheet
-f = open('app/imports/assets/records.json')
+f = open('./records.json')
 for rec in json.load(f):
     p1 = rec['p1']
     if p1 not in records:
@@ -46,8 +47,44 @@ for rec in json.load(f):
 def print_rec(rec, player):
     print(player, "beat" if rec.result == 'win' else "lost to", rec.opponent, rec.my_score, '-', rec.opp_score)
 
+# Check if two players have played before
+def comp(p1, p2):
+    # Check if they have played before
+    for rec in records[p1]:
+        if rec.opponent == p2:
+            print_rec(rec, p1)
+
 # BFS to compare 2 players
 def bfs_comp(p1, p2):
+    q = []
+    for rec in records[p1]:
+        q.append((rec, []))
+        print_rec(rec, p1)
+
+    visited = []
+    while (q):
+        rec, opps = q.pop(0)
+
+        if rec.opponent in visited:
+            continue
+        visited.append(rec.opponent)
+        opps.append(rec.opponent)
+
+        if (rec.opponent == p2):
+            print(p1, 'has a', len(opps), 'degree', rec.result, 'against', p2)
+            print(opps)
+            curr = p1
+            for opp in opps:
+                comp(curr, opp)
+                curr = opp
+            break
+
+        for next in records[rec.opponent]:
+            if next.result == rec.result:
+                print_rec(next, rec.opponent)
+                q.append((next, opps))
+
+def bfs_comp_val(p1, p2):
     q = []
     for rec in records[p1]:
         q.append((rec, 1))
@@ -61,17 +98,30 @@ def bfs_comp(p1, p2):
         visited.append(rec.opponent)
 
         if (rec.opponent == p2):
-            print(p1, 'has a', degrees, 'degree', rec.result, 'against', p2)
-            break
+            return -1 if rec.result == 'win' else 1
 
         for next in records[rec.opponent]:
             if next.result == rec.result:
                 q.append((next, degrees + 1))
 
+    return win_count(p2) - win_count(p1)
+
+# players = ['Nikhil Aadapu', 'Sourodeep Deb', 'Warren Zhou', 'Charles Wang', 'Neev Fnu', 'Sreesh Srinivasan', 'Aaron Li', 'Rishabh Shah', 'Lakshmi Prasanna', 'Sairisheeth Venkat', 'Sohum Waduwani', 'Sai Divyesh Tunguturu']
+# players.sort(key=functools.cmp_to_key(bfs_comp_val))
+# for player in players:
+#     print(player, win_count(player))
+
+bfs_comp('Nikhil Aadapu', 'Sairisheeth Venkat')
+
+# for p1 in players:
+#     for p2 in players:
+#         if p1 != p2:
+#             bfs_comp(p1, p2)
+
 # Interactive program
 while True:
     line = input()
-    if line == 'quit':
+    if line == 'q':
         break
     args = str(line).split()
 
@@ -92,11 +142,7 @@ while True:
         # Number of wins
         print(p1, 'has', win_count(p1), 'wins')
         print(p2, 'has', win_count(p2), 'wins')
-        
-        # Check if they have played before
-        for rec in records[p1]:
-            if rec.opponent == p2:
-                print_rec(rec, p1)
+        comp(p1, p2)
 
         # Check if they can be indirectly compared
         bfs_comp(p1, p2)
