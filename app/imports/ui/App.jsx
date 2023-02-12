63d0324505a5c1a085fe6218d9b610f5d674aacb
mod3sty <marco.yang.ca@gmail.com>
2023-02-11 17:07:15 -0800

    better ui

--- app/imports/ui/App.jsx
+++ app/imports/ui/App.jsx
@@ -1,10 +1,62 @@
-import React from 'react';
-import MatchTable from './components/table';
+import React, {useState} from 'react';
 import MatchList from './components/match-list'
+import {GiTennisRacket} from 'react-icons/gi'
 
-export const App = () => (
-  <div>
-    <h1>MSJ Boys Tennis Tryouts</h1>
-    <MatchList/>
-  </div>
-);
+export const App = () => {
+
+    const [display, setDisplay] = useState('match-list')
+
+    const displayingMatches = display == 'match-list'
+    const displayingLb = display == 'leaderboard'
+    const displayingComp = display == 'comparison'
+
+    const returnDisplay = () => {
+        if (displayingMatches) {
+            return (<MatchList/>)
+        } else if (displayingLb) {
+            return ('')
+        } else if (displayingComp) {
+            return ('')
+        }
+    }
+
+    return (
+      <div className=''>
+
+        <div className='vertical-center'>
+        </div>
+
+        <div className='vertical-center'>
+            <GiTennisRacket className='margin-right' style={{fontSize: '64px'}}/>
+            <h1> msj tennis </h1>
+        </div>
+
+        <div id="navbar-container">
+            <div id="navbar" class="row rounded black-shadow vertical-center">
+                <div 
+                    className='row-element nav-element rounded-left active' 
+                    style={{backgroundColor: displayingMatches ? 'var(--accent)': 'white', color: displayingMatches ? 'white' : 'black'}} 
+                    onClick={() => {setDisplay('match-list')}}>
+                    Match List
+                </div>
+                <div 
+                    className='row-element nav-element' 
+                    style={{backgroundColor: displayingLb ? 'var(--accent)': 'white', color: displayingLb ? 'white' : 'black'}} 
+                    onClick={() => {setDisplay('leaderboard')}}>
+                    Leaderboard
+                </div>
+                <div 
+                    className='row-element nav-element rounded-right' 
+                    style={{backgroundColor: displayingComp ? 'var(--accent)': 'white', color: displayingComp ? 'white' : 'black'}} 
+                    onClick={() => {setDisplay('comparison')}}>
+                    Comparisons
+                </div>
+            </div>
+        </div>
+
+        <div style={{height: '50px'}}></div>
+
+        {returnDisplay()}
+      </div>
+    )
+};
--- app/imports/ui/components/match-list.jsx
+++ app/imports/ui/components/match-list.jsx
@@ -1,54 +1,98 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useTracker } from 'meteor/react-meteor-data'
 import { MatchCollection } from '/imports/api/collections.js'
+import {BiSearch} from 'react-icons/bi'
+import {TbArrowsSort} from 'react-icons/tb'
 
 export default function MatchList() {
-    
-    const matches = useTracker(() => MatchCollection.find().fetch())
+
+    const [searchedName, setSearchedName] = useState("")
+    const [sortReverse, setSortReverse] = useState(true)
+
+    const all_matches = useTracker(() => MatchCollection.find().fetch())
+
+    const matches = all_matches.filter(match => match.p1.includes(searchedName)|| match.p2.includes(searchedName))
+
+    const dateToString = (date) => {
+        return new Date(date).toLocaleString('en-US', {dateStyle: 'medium'}) 
+    }
 
     const rows = matches.map(
         (match) => (
             <div className='list-entry black-shadow rounded'>
-                <div className='list-entry-left' style={{'--color': match.s1 > match.s2 ? '#009879' : 'red'}}>
+                <div className='list-entry-left rounded-left' style={{'--color': match.s1 > match.s2 ? 'var(--green)' : 'var(--red)'}}>
                     {match.p1}
                 </div>
-                <div className='list-entry-center rounded'>
-                    {match.s1 + ' - ' + match.s2}
+                <div className='list-entry-center rounded horizontal-center'>
+                    <div>{match.s1 + ' - ' + match.s2}</div> 
+                    <div style={{height: '5px'}}></div>
+                    <div className='match-date'>{dateToString(match.date)}</div>
                 </div>
-                <div className='list-entry-right' style={{'--color': match.s1 > match.s2 ? 'red' : '#009879'}}>
+                <div className='list-entry-right rounded-right' style={{'--color': match.s1 > match.s2 ? 'var(--red)' : 'var(--green)'}}>
                     {match.p2}
                 </div>
             </div>
         )
     )
+
+    const sortFn = (m1, m2) => {
+        sortReverse ? new Date(m2.date) - new Date(m1.date) : new Date(m1.date) - new Date(m2.date)
+    }
+
+    rows.sort(sortFn)
     
     const handleAddMatch = () => {
         const p1 = document.getElementById('p1').value
         const p2 = document.getElementById('p2').value
         const s1 = document.getElementById('s1').value
         const s2 = document.getElementById('s2').value
+        let date = document.getElementById('match-date').value
+        const today = new Date()
+        if (dateToString(new Date(date)) == dateToString(today)) {
+            date = today
+        }
         
         const new_rec = {
             p1: p1,
             p2: p2,
             s1: s1,
-            s2: s2
+            s2: s2,
+            date: date
         }
         
         MatchCollection.insert(new_rec)
     }
 
+    const handleSearch = (event) => {
+        setSearchedName(event.target.value)
+    }
+
+    const handleReverse = () => {
+        setSortReverse(!sortReverse)
+    }
+
     return (
-        <div className='rounded'>
+        <div className='centered'> 
             <div className='row'>
-                <input className='row-element black-shadow' type='text' id='p1' name='p1-input' placeholder='Player 1 Name'  />
-                <input className='row-element black-shadow' type='text' id='p2' name='p2-input' placeholder='Player 2 Name' />
-                <input className='row-element black-shadow' type='text' id='s1' name='s1-input' placeholder='Player 1 Score'/>
-                <input className='row-element black-shadow' type='text' id='s2' name='s2-input' placeholder='Player 2 Score'/>
-                <button className='row-element black-shadow' onClick={handleAddMatch}>Add Match</button>
+                <div id='match-input-container' className='row rounded-left grey-outline' style={{borderRight: 'none'}}>
+                    <input className='row-element rounded-left' type='text' id='p1' name='p1-input' placeholder='Player 1 Name'  />
+                    <input className='row-element' type='text' id='p2' name='p2-input' placeholder='Player 2 Name' />
+                    <input className='row-element' type='text' id='s1' name='s1-input' placeholder='Player 1 Score'/>
+                    <input className='row-element' type='text' id='s2' name='s2-input' placeholder='Player 2 Score'/>
+                    <input className='row-element' type='date' id='match-date' name='match-date-input' defaultValue={new Date()}/>
+                </div>
+                <button id='add-match' className='row-element rounded-right margin-right' style={{height: '52px'}} onClick={handleAddMatch}>Add Match</button>
+
+                <div id='match-search-container' className='row rounded grey-outline margin-right'>
+                    <div className='vertical-center'>
+                        <BiSearch className='row-element' style={{fontSize: '1.1em', color: 'grey'}}/>
+                        <input className='row-element rounded' type='text' id='player-search' name='player-search' onChange={handleSearch} placeholder="Search for player's matches"/>
+                    </div>
+                </div>
+                <button id='reverse-sort-btn' className='row-element rounded margin-right' style={{fontSize: '1em'}} onClick={handleReverse}> <TbArrowsSort/> </button>
             </div>
         
-            <div style={{height: '50px'}}></div>
+            <div style={{height: '80px'}}></div>
         
             <div className='list'>
                 {rows}
