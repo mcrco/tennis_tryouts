dbd2e735ab23bb28d5ebb460b870053f25bcf35c
mod3sty <marco.yang.ca@gmail.com>
2023-12-16 14:57:40 -0800

    finished home page + favicon/name + removed unused files

--- app/imports/ui/App.jsx
+++ app/imports/ui/App.jsx
@@ -20,7 +20,7 @@ export const App = () => {
     // Return login form if user isn't logged in
     else if (user == undefined) {
         return (
-            <div className='flex flex-col items-start space-y-8'>
+            <div className='flex flex-col items-start space-y-16'>
                 <div className='w-full flex flex-row justify-between items-center'>
 
                     <h1> Tennis Ranker </h1>
@@ -39,8 +39,51 @@ export const App = () => {
                     whether its for tryouts or a tournament.
                 </span>
 
+                {/* <div className='flex flex-col space-y-16 px-8 py-4 shadow-md rounded-lg'> */}
+                <h2> Features </h2>
+
+                <div className='flex flex-col space-y-16 items-center'>
+                    <div className='flex flex-row justify-between items-start pb-16 border-b border-gray-200'>
+                        <div className='w-1/2 text-left'>
+                            <h3 className='mb-4'> Coach Dashboard </h3>
+                            Manage multiple sessions for different ocassions, share results with others.
+                        </div>
+                        <div className='p-8 border border-gray-200 rounded-lg shadow-lg w-1/2'>
+                            <img src='sessions.png' className='w-full' />
+                        </div>
+                    </div>
+                    <div className='flex flex-row justify-between items-start pb-16 border-b border-gray-200'>
+                        <div className='w-1/2 text-left'>
+                            <h3 className='mb-4'> Match List </h3>
+                            View matches in every session, filter matches by player.
+                        </div>
+                        <div className='p-8 border border-gray-200 rounded-lg shadow-lg w-1/2'>
+                            <img src='matchlist.png' className='w-full' />
+                        </div>
+                    </div>
+                    <div className='flex flex-row justify-between items-start pb-16 border-b border-gray-200'>
+                        <div className='w-1/3 text-left'>
+                            <h3 className='mb-4'> Leaderboard </h3>
+                            View your players by wins or by rank, determined via our comparison algorithm (more about this in the next feature).
+                        </div>
+                        <div className='p-8 border border-gray-200 rounded-lg shadow-lg w-1/2'>
+                            <img src='leaderboard.png' className='w-full' />
+                        </div>
+                    </div>
+                    <div className='flex flex-row justify-between items-start'>
+                        <div className='w-1/3 text-left'>
+                            <h3 className='mb-4'> Compare Players </h3>
+                            Compare two players who have (or haven't!) played with each other. The app automatically finds "win chains," demonstrated on the right.
+                        </div>
+                        <div className='p-8 border border-gray-200 rounded-lg shadow-lg w-1/2'>
+                            <img src='compare.png' className='w-full' />
+                        </div>
+                    </div>
+                </div>
+
                 <PopupModal visible={showLoginModal} body={<LoginForm />} toggleVisible={() => setShowLoginModal(false)} />
                 <PopupModal visible={showSessionFinderModal} body={<SessionFinder setSessionCode={setSessionCode} />} toggleVisible={() => setShowSessionFinderModal(false)} />
+                {/* </div> */}
             </div>
         )
     }
